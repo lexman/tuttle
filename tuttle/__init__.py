a0c99ec379c681e8657310510883f08fa6365e5f
Lexman <tuttle@lexman.org>
2015-02-01 12:42:28 +0100

    Cleanup in main (not usable yet)

--- tuttle/__init__.py
+++ tuttle/__init__.py
@@ -1,5 +1,2 @@
 #!/usr/bin/env python
 # -*- coding: ascii -*-
-
-def test():
-    print "Test"
--- tuttle/main.py
+++ tuttle/main.py
@@ -1,13 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
 import sys
-from tuttle import test
+from optparse import OptionParser
+
 
 def main():
-    """Main entry point for the script."""
+    """Main entry point for the project."""
+    usage = 'bin/project'
+    parser = OptionParser(usage=usage)
+    parser.add_option('-x', '--example',
+                  default='example-value',
+                  dest='example',
+                  help='An example option')
     print "Main"
-    #print tuttle.__file__
-    test()
-	
-	
 
 if __name__ == '__main__':
     sys.exit(main())
