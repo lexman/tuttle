1fe6f5946a4f95d1db8cb6f4274edf7fd297c95f
Lexman <tuttle@lexman.org>
2015-02-05 21:37:08 +0100

    Removed useless code

--- tuttle/workflow.py
+++ tuttle/workflow.py
@@ -15,18 +15,6 @@ class Workflow:
         """
         self.processes.append(process)
 
-
-    def raise_if_missing_inputs(self):
-        """Raises a Workflow exception if some inputs are missing"""
-        missing_inputs = self.missing_inputs()
-        if len(missing_inputs) > 0 :
-            missing_txt = ""
-            for missing in missing_inputs:
-                missing_txt = missing_txt + "* {}\n".format(missing)
-            raise WorkflowError("Some resources that are inputs of the workflow "
-                                 "are missing : \n{}".format(missing_txt))
-
-
     def get_a_process_to_run(self):
         """ Pick up a process to run
         :return:
