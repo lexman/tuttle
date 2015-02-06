027646caa2b4c149932a20980ab6899de698a5da
Lexman <tuttle@lexman.org>
2015-02-06 10:57:22 +0100

    The parser can be launched from command line !
    Just type "tuttle" inside a project directory. Ok it won't do much for the moment, except telling you the missing input resources...

--- tuttle/__init__.py
+++ tuttle/__init__.py
@@ -3,3 +3,26 @@
 
 __version__ ='0.1'
 
+from project_parser import ProjectParser, ParsingError
+from os import chdir, getcwd
+from os.path import abspath
+
+def prepare_workflow(tuttlefile, workspace):
+    curdir = getcwd()
+    abs_tuttlefile = abspath(tuttlefile)
+    chdir(workspace)
+    try:
+        pp = ProjectParser()
+        try:
+            pp.parse_file(abs_tuttlefile)
+        except ParsingError as e:
+            print e
+        missing = pp.missing_inputs()
+        if missing:
+            error_msg = "Missing the following resources to launch the workflow :\n"
+            for mis in missing:
+                error_msg += "* {}\n".format(mis.url)
+            print error_msg
+    except BaseException:
+        chdir(curdir)
+        raise
