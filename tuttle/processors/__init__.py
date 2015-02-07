292ee2c42508fdbbe89f1d037689335c19b06341
Lexman <tuttle@lexman.org>
2015-02-07 20:37:53 +0100

    Refactored shel processor : extracted the subprocesss part into a standalone function

--- tuttle/processors/__init__.py
+++ tuttle/processors/__init__.py
@@ -5,24 +5,28 @@ from os import path, chmod, makedirs, stat
 from stat import S_IXUSR, S_IXGRP, S_IXOTH
 from subprocess import Popen, PIPE
 
+def run_and_log(prog, log_stdout, log_stderr):
+    osprocess = Popen([prog], stdout=PIPE, stderr=PIPE)
+    stdout, stderr = osprocess.communicate()
+    with open(log_stdout, 'w') as f:
+        f.write(stdout)
+    with open(log_stderr, 'w') as f:
+        f.write(stderr)
+    return stdout, stderr
+
+
 class ShellProcessor:
     """ A processor runs process code
     """
     name = 'shell'
-
-
     header = """
 echo "Shell process"
 """
 
-
-    def __init__(self):
-        pass
-
     def generate_executable(self, code, line_num, directory):
         """ Create an executable file
         :param directory: string
-        :return: the path of the file
+        :return: the path to the file
         """
         script_name = path.join(directory, "shell_{}.bat".format(line_num))
         with open(script_name, "w+") as f:
@@ -33,13 +37,10 @@ echo "Shell process"
         return script_name
 
     def run(self, script_path, logs_dir):
-        osprocess = Popen([path.abspath(script_path)], stdout=PIPE, stderr=PIPE)
-        stdout, stderr = osprocess.communicate()
-        print stdout
+        prog = path.abspath(script_path)
         script_name = path.basename(script_path)
         log_stdout = path.join(logs_dir, "{}_stdout".format(script_name))
-        with open(log_stdout, 'w') as f:
-            f.write(stdout)
         log_stderr = path.join(logs_dir, "{}_err".format(script_name))
-        with open(log_stderr, 'w') as f:
-            f.write(stderr)
+        stdout, stderr = run_and_log(prog, log_stdout, log_stderr)
+        print stdout
+        print stderr
