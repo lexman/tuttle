6613495425ef08700b7960ca83289590a2269ecd
Lexman <tuttle@lexman.org>
2015-02-07 22:17:47 +0100

    Now pick_a_process_to_run() tries to pick a process in the right order
     Still it is not tested, nor robust to circular dependencies. And if a process has no output, it can be picked up endlessly

--- tuttle/workflow.py
+++ tuttle/workflow.py
@@ -19,7 +19,15 @@ class Workflow:
         """ Pick up a process to run
         :return:
         """
+        #TODO : check for circular references
         for process in self.processes:
+            # All outputs are supposed to be generated at the same time with a process,
+            # so checking for existence of one is like checking fo existence of all !
             if len(process._outputs) > 0 and not process._outputs[0].exists():
+                for in_res in process._inputs:
+                    if not in_res.exists():
+                        # Can't pick this one if all dependencies have not been generated !
+                        continue
+                # Every input is here, so the process can be run !
                 return process
         return None
