d9277f94e8e8be2f2288227ebffaa2f7666e885d
Lexman <tuttle@lexman.org>
2015-02-14 13:22:16 +0100

    Small refactor

--- tuttle/report/dot_repport.py
+++ tuttle/report/dot_repport.py
@@ -32,9 +32,9 @@ def create_dot_report(workflow, filename):
             fout.write('    {} [shape="none", label="", width=0, height=0] ;\n'.format(p_node))
             for res_input in process._inputs:
                 fout.write('    "{}" -> {} [arrowhead="none"] \n'.format(workflow.nick_from_url(res_input.url), p_node))
+            color = resource_color_from_process_state(process.get_state())
             for res_output in process._outputs:
                 nick = workflow.nick_from_url(res_output.url)
-                color = resource_color_from_process_state(process.get_state())
                 fout.write('    {} -> "{}" \n'.format(p_node, nick))
                 fout.write('    "{}" [fillcolor={}] ;\n'.format(nick, color))
         fout.write('}')
