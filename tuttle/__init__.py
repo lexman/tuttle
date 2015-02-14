1f0092907ae2282288c4cf4e226da0649d736a35
Lexman <tuttle@lexman.org>
2015-02-14 12:38:32 +0100

    Refactored the responsibilities between main script and main package
    * The main script bin/tuttle is responsible for path of files and directories
    * All business logic package main :
          - parsing the workflow and displaying error messages
          - check consistency of the workflow
          - prepare and rune the workflow

--- tuttle/__init__.py
+++ tuttle/__init__.py
@@ -4,30 +4,42 @@
 __version__ ='0.1'
 
 from project_parser import ProjectParser, ParsingError
-from os import chdir, getcwd
-from os.path import abspath
 
-def prepare_workflow(tuttlefile, workspace):
-    curdir = getcwd()
-    abs_tuttlefile = abspath(tuttlefile)
-    chdir(workspace)
+
+def prepare_workflow(tuttlefile):
+    pp = ProjectParser()
     try:
-        pp = ProjectParser()
-        try:
-            workflow = pp.parse_file(abs_tuttlefile)
-        except ParsingError as e:
-            print e
-            return
-        missing = workflow.missing_inputs()
-        if missing:
-            error_msg = "Missing the following resources to launch the workflow :\n"
-            for mis in missing:
-                error_msg += "* {}\n".format(mis.url)
-            print error_msg
-        else:
-            return workflow
-    finally:
-        chdir(curdir)
+        workflow = pp.parse_file(tuttlefile)
+    except ParsingError as e:
+        print e
+        return
+
+    missing = workflow.missing_inputs()
+    if missing:
+        error_msg = "Missing the following resources to launch the workflow :\n"
+        for mis in missing:
+            error_msg += "* {}\n".format(mis.url)
+        print error_msg
+    else:
+        return workflow
+
+
+def abort_if_workflow_inconsistent(workflow):
+    """ Check consistency of the workflow and display needed error messages.
+
+    :param workflow:
+    :return: False if workflow is correct, True if workflow is inconsistent
+    """
+    missing = workflow.missing_inputs()
+    if missing:
+        error_msg = "Missing the following resources to launch the workflow :\n"
+        for mis in missing:
+            error_msg += "* {}\n".format(mis.url)
+        print error_msg
+        return True
+    return False
+
+
 
 def run_workflow(workflow):
     """ Runs all the needed processes in a workflow
