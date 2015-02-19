3407c21faf54833edb2ac941410a99f1d6d06aea
Lexman <tuttle@lexman.org>
2015-02-19 15:36:04 +0100

    Very basic test

---
+++ tests/functional_tests/test_remove_resource/test_create_resource.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from os import path
+from tests.functional_tests import FunctionalTestBase
+
+
+class TestCreateResource(FunctionalTestBase):
+
+    def test_create_resource(self):
+        """ When launching "tuttle" in the command line, should produce the result"""
+        self.work_dir_from_module(__file__)
+        first = """file://B <- file://A
+    echo A produces B
+    echo B > B
+
+"""
+        self.write_tuttlefile(first)
+        result = self.run_tuttle()
+        assert path.exists('B')
