2370d55fa126a802b09e11840acef87db171f26e
Lexman <tuttle@lexman.org>
2015-03-29 00:30:19 +0100

    Improved DOT reporting

--- tuttle/report/dot_repport.py
+++ tuttle/report/dot_repport.py
@@ -6,16 +6,20 @@ DOT_HEADER = """digraph workflow {
     Node [style="rounded,filled", shape=box, fillcolor=none]
 """
 
-def resource_color_from_process_state(process_state):
-    if process_state == ProcessState.COMPLETE:
-        return "green"
-    elif process_state == ProcessState.ERROR:
-        return "red"
-    elif process_state == ProcessState.RUNNING:
-        return "skyblue"
-    else:
-        return "none"
 
+def color_from_process(process):
+    color = "none"
+    if process.start:
+        if not process.end:
+            # Running
+            color = "skyblue"
+        elif not process.return_code:
+            # success
+            color = "green"
+        else:
+            color = "red"
+
+    return color
 
 def create_dot_report(workflow, filename):
     """ Write to disk a dot file describing the workflow, with color for states
@@ -32,7 +36,7 @@ def create_dot_report(workflow, filename):
             fout.write('    {} [shape="none", label="", width=0, height=0] ;\n'.format(p_node))
             for res_input in process._inputs:
                 fout.write('    "{}" -> {} [arrowhead="none"] \n'.format(workflow.nick_from_url(res_input.url), p_node))
-            color = resource_color_from_process_state(process.get_state())
+            color = color_from_process(process)
             for res_output in process._outputs:
                 nick = workflow.nick_from_url(res_output.url)
                 fout.write('    {} -> "{}" \n'.format(p_node, nick))
