dbf0a9d0dc721945aa02be3f30f66f990b6d6b78
Lexman <tuttle@lexman.org>
2015-03-28 23:47:56 +0100

    Nicer html repport

--- tuttle/report/html_repport.py
+++ tuttle/report/html_repport.py
@@ -3,6 +3,33 @@
 
 from jinja2 import Template
 from os import path
+from time import strftime, localtime
+
+
+def format_process(process):
+    duration = ""
+    start = ""
+    end = ""
+    return_code = ""
+    if process.start:
+        start = strftime("%a, %d %b %Y %H:%M:%S", localtime(process.start))
+        if process.end:
+            end = strftime("%a, %d %b %Y %H:%M:%S", localtime(process.end))
+            duration = process.end - process.start
+            return_code = process.return_code
+
+    return {
+        'id' : process.id(),
+        'start' : start,
+        'end' : end,
+        'duration' : duration,
+        'log_stdout' : process.log_stdout,
+        'log_stderr' : process.log_stderr,
+        '_outputs' : process._outputs,
+        '_inputs' : process._inputs,
+        '_code' : process._code,
+        'return_code' : return_code,
+    }
 
 
 def create_html_report(workflow, filename):
@@ -15,5 +42,6 @@ def create_html_report(workflow, filename):
     tpl_filename = path.join(module_dir, "report_template.html")
     with open(tpl_filename, "r") as ftpl:
         t = Template(ftpl.read())
+    processes = [format_process(p) for p in workflow.processes]
     with open(filename, "w") as fout:
-        fout.write(t.render(processes = workflow.processes))
+        fout.write(t.render(processes = processes, ))
