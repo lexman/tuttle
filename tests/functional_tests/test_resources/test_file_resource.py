8287876963af72756c3ff9102526c56f3e28a8a2
Lexman <tuttle@lexman.org>
2015-04-03 21:10:00 +0200

    Test for file resources

---
+++ tests/functional_tests/test_resources/test_file_resource.py
@@ -0,0 +1,19 @@
+# -*- coding: utf8 -*-
+
+
+from tuttle.resources import FileResource
+import tuttle.resources
+from os import path
+
+class TestHttpResource():
+
+    def test_real_resource_exists(self):
+        """A real resource should exist"""
+        file_url = "file://{}".format(path.abspath(tuttle.resources.__file__))
+        res = FileResource(file_url)
+        assert res.exists()
+
+    def test_fictive_resource_exists(self):
+        """A real resource should exist"""
+        res = FileResource("fictive_file")
+        assert not res.exists()
