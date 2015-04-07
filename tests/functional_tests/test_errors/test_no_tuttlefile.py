bcf7ba59da3aab6ce3c6a3141b1f2c5fcd1db36d
Lexman <tuttle@lexman.org>
2015-04-07 21:58:16 +0200

    Running tuttle with no tuttlefile exits with status code 0, like make + test

--- tests/functional_tests/test_errors/test_no_tuttlefile.py
+++ tests/functional_tests/test_errors/test_no_tuttlefile.py
@@ -1,20 +1,31 @@
 # -*- coding: utf-8 -*-
 
-from subprocess import check_output
-from tests.functional_tests import FunctionalTestBase
+from subprocess import check_output, Popen, PIPE
+from os.path import abspath, join, dirname
+from tests.functional_tests import isolate
 
-class TestNoTuttlefile(FunctionalTestBase):
 
+class TestNoTuttlefile():
+
+    @isolate
     def test_no_file_in_current_dir(self):
         """ Should display a message if there is no tuttlefile in the current directory"""
-        self.work_dir_from_module(__file__)
-        code, output = self.run_tuttle()
+        dir = dirname(__file__)
+        tuttle_cmd = abspath(join(dir, '..', '..', '..', 'bin', 'tuttle'))
+        proc = Popen(['python', tuttle_cmd], stdout=PIPE)
+        output = proc.stdout.read()
+        rcode = proc.wait()
         # TODO : shouldn't we exit with error code ?
-        assert code == 0
+        assert rcode == 2
         assert output.find("No tuttlefile") >= 0
 
 
     def test_tuttle_file_does_not_exist(self):
         """ Should display a message if the tuttlefile passed as argument to the command line does not exist"""
-        result = check_output(['python', self._tuttle_cmd, '-f', 'inexistant_file'])
-        assert result.find("No tuttlefile") >= 0
+        dir = dirname(__file__)
+        tuttle_cmd = abspath(join(dir, '..', '..', '..', 'bin', 'tuttle'))
+        proc = Popen(['python', tuttle_cmd, '-f', 'inexistant_file'], stdout=PIPE)
+        output = proc.stdout.read()
+        rcode = proc.wait()
+        assert rcode == 2
+        assert output.find("No tuttlefile") >= 0
