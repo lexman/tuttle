f3fac1e23fac42c5c81847a798eda02540a1d3d9
Lexman <tuttle@lexman.org>
2015-05-03 00:50:43 +0200

    Improved imports in tuttle.py in order to work with cx-freeze and pip

--- tests/functional_tests/test_errors/test_no_tuttlefile.py
+++ tests/functional_tests/test_errors/test_no_tuttlefile.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from subprocess import check_output, Popen, PIPE
+from subprocess import Popen, PIPE
 from os.path import abspath, join, dirname
 from tests.functional_tests import isolate
 
@@ -11,20 +11,17 @@ class TestNoTuttlefile():
     def test_no_file_in_current_dir(self):
         """ Should display a message if there is no tuttlefile in the current directory"""
         dir = dirname(__file__)
-        #tuttle_cmd = abspath(join(dir, '..', '..', '..', 'bin', 'tuttle'))
-        tuttle_cmd = abspath(join(dir, '..', '..', '..', 'tuttle.py'))
+        tuttle_cmd = abspath(join(dir, '..', '..', '..', 'bin', 'tuttle'))
         proc = Popen(['python', tuttle_cmd], stdout=PIPE)
         output = proc.stdout.read()
         rcode = proc.wait()
         assert rcode == 2
         assert output.find("No tuttlefile") >= 0
 
-
     def test_tuttle_file_does_not_exist(self):
         """ Should display a message if the tuttlefile passed as argument to the command line does not exist"""
         dir = dirname(__file__)
         tuttle_cmd = abspath(join(dir, '..', '..', '..', 'bin', 'tuttle'))
-        tuttle_cmd = abspath(join(dir, '..', '..', '..', 'tuttle.py'))
         proc = Popen(['python', tuttle_cmd, '-f', 'inexistant_file'], stdout=PIPE)
         output = proc.stdout.read()
         rcode = proc.wait()
