bb93d53e4725b86e4b93d81464c7c26020b09786
Lexman <tuttle@lexman.org>
2015-06-05 13:45:38 +0200

    Not sure about this test : if a process without output fails, does tuttle have to re-run it, or say immediately that it has failed ?

--- tests/functional_tests/test_errors/test_error_in_process.py
+++ tests/functional_tests/test_errors/test_error_in_process.py
@@ -81,10 +81,31 @@ file://D <- file://A
         assert rcode == 2
         assert output.find("Workflow already failed") >= 0, output
 
+    # Not sure about that
+#     @isolate(['A'])
+#     def test_fail_if_already_failed_even_without_outputs(self):
+#         """  When a process fail, Tuttle should exit with status code 2, even if the process has no outputs"""
+#         project = """file://B <- file://A
+#     echo A produces B
+#     echo B > B
+#
+# <- file://B
+#     error
+#     echo This should not be written
+#     echo C > C
+# """
+#         rcode, output = run_tuttle_file(project)
+#         assert rcode == 2
+#         assert isfile('B')
+#         assert not isfile('C')
+#
+#         rcode, output = run_tuttle_file(project)
+#         assert rcode == 2
+#         assert output.find("Workflow already failed") >= 0, output
 
     @isolate(['A'])
-    def test_process_fail_if_output_not_create(self):
-        """  If the all the outputs of a process have not been create, the process should be marked as failed
+    def test_process_fail_if_output_not_created(self):
+        """  If the all the outputs of a process have not been created, the process should be marked as failed
         even if no error occurred.
         Useful when displaying html report
         """
