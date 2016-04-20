37c261c9c953c6545a179b798f32ae481048c892
Lexman <tuttle@lexman.org>
2016-04-20 16:46:23 +0200

    'tuttle-extend-workflow' can inject variables in template

--- tests/functional_tests/test_extend_workflow.py
+++ tests/functional_tests/test_extend_workflow.py
@@ -1,24 +1,24 @@
 # -*- coding: utf-8 -*-
-from subprocess import Popen, PIPE, check_output
+from subprocess import Popen, PIPE, check_output, CalledProcessError
 from os.path import isfile, dirname, abspath, join
 
 from os import path, environ, getcwd
 from tests.functional_tests import isolate, run_tuttle_file
 from shlex import split
+from pipes import quote
 
 
 class TestExtendWorkflow:
 
-    def get_cmd_extend_workflow(self):
+    def get_cmd_extend(self, args_st):
         """
         :return: A command line to call tuttle-extend-workflow even if tuttle has not been installed with pip
         """
-        if environ.has_key('VIRTUAL_ENV'):
-            py_cli = abspath(join(environ['VIRTUAL_ENV'], 'Scripts', 'python'))
-        else:
-            py_cli = 'python'
-        extend = abspath(join(__file__, '..', '..', '..', 'bin', 'tuttle-extend-workflow'))
-        cmd_extend = "{} {}".format(py_cli, extend)
+        #if environ.has_key('VIRTUAL_ENV'):
+        #    py_cli = abspath(join(environ['VIRTUAL_ENV'], 'Scripts', 'python'))
+        py_cli = 'python'
+        extend = quote(abspath(join(__file__, '..', '..', '..', 'bin', 'tuttle-extend-workflow')))
+        cmd_extend = "{} {} {}".format(py_cli, extend, args_st)
         return cmd_extend
 
     def init_tuttle_project(self):
@@ -28,11 +28,36 @@ class TestExtendWorkflow:
         rcode, output = run_tuttle_file(project)
         assert rcode == 0, output
 
-    @isolate(['A'])
-    def test_create_extension(self):
-        """ A preprocess should be able to call the tuttle-extend-workflow command"""
+    @isolate(['A', 'b-produces-x.tuttle'])
+    def test_create_extension_file(self):
+        """ Calling tuttle-extend-workflow command creates an extension file in the right directory"""
         self.init_tuttle_project()  # ensures there is a .tuttle directory
         extend = abspath(join(__file__, '..', '..', '..', 'bin', 'tuttle-extend-workflow'))
-        output = check_output(['python', extend, 'b-produces-x', 'x="C"'])
+        output = check_output(['python', extend, 'b-produces-x.tuttle', 'x="C"'])
         expected_file = join('.tuttle', 'extensions', 'extension_1')
         assert isfile(expected_file), output
+
+    @isolate(['A', 'b-produces-x.tuttle'])
+    def test_create_extension_from_template(self):
+        """ A call the tuttle-extend-workflow command creates an extension file and injects variables """
+        self.init_tuttle_project()  # ensures there is a .tuttle directory
+        cmd = self.get_cmd_extend('b-produces-x.tuttle x="C"')
+        output = check_output(split(cmd))
+        expected_file = join('.tuttle', 'extensions', 'extension_1')
+        assert isfile(expected_file), output
+        extension = open(expected_file).read()
+        rule_pos = extension.find("file://C <- file://B")
+        assert rule_pos > -1, extension
+
+    @isolate(['A', 'b-produces-x.tuttle'])
+    def test_bad_extension_parameter(self):
+        """ When the syntax of parameters for tuttle-extend-workflow is wrong it should fail"""
+        self.init_tuttle_project()  # ensures there is a .tuttle directory
+        cmd = self.get_cmd_extend('b-produces-x.tuttle x"C"')
+        try:
+            output = check_output(split(cmd))
+            assert False, "tuttle-extend-workflow should have exited in error"
+        except CalledProcessError as e:
+            assert e.returncode == 1
+            pos_err = e.output.find('Can\'t extract variable from parameter')
+            assert pos_err > -1, e.output
