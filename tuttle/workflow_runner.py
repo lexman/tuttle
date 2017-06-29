cf11ca3e1f46603fb6275a35b4f2c01b5f50265f
lexman <android@lexman.org>
2017-06-29 07:06:54 +0200

    Mark unfinished process as failure and move some code to the ed of the file

--- tuttle/workflow_runner.py
+++ tuttle/workflow_runner.py
@@ -49,87 +49,6 @@ class WorkflowRuner():
         return join('.tuttle', *args)
 
     @staticmethod
-    def prepare_and_assign_paths(process):
-        log_stdout = join(WorkflowRuner._logs_dir, "{}_stdout.txt".format(process.id))
-        log_stderr = join(WorkflowRuner._logs_dir, "{}_err.txt".format(process.id))
-        # It would be a good idea to clean up all directories before
-        # running the whole workflow
-        # For the moment we clean here : before folowing the logs
-        if isfile(log_stdout):
-            remove(log_stdout)
-        if isfile(log_stderr):
-            remove(log_stderr)
-        reserved_path = join(WorkflowRuner._processes_dir, process.id)
-        if isdir(reserved_path):
-            rmtree(reserved_path)
-        elif isfile(reserved_path):
-            remove(reserved_path)
-        process.assign_paths(reserved_path, log_stdout, log_stderr)
-
-    @staticmethod
-    def create_tuttle_dirs():
-        if not isdir(WorkflowRuner._processes_dir):
-            makedirs(WorkflowRuner._processes_dir)
-        if not isdir(WorkflowRuner._logs_dir):
-            makedirs(WorkflowRuner._logs_dir)
-
-    @staticmethod
-    def empty_extension_dir():
-        if not isdir(WorkflowRuner._extensions_dir):
-            makedirs(WorkflowRuner._extensions_dir)
-        else:
-            rmtree(WorkflowRuner._extensions_dir)
-            makedirs(WorkflowRuner._extensions_dir)
-
-    @staticmethod
-    def print_header(process, logger):
-        logger.info("=" * 60)
-        logger.info(process.id)
-        logger.info("=" * 60)
-
-    @staticmethod
-    def print_preprocess_header(process, logger):
-        logger.info("-" * 60)
-        logger.info("Preprocess : {}".format(process.id))
-        logger.info("-" * 60)
-
-    @staticmethod
-    def print_preprocesses_header():
-        print "=" * 60
-        print "Running preprocesses for this workflow"
-        print "=" * 60
-
-    @staticmethod
-    def print_preprocesses_footer():
-        print "=" * 60
-        print "End of preprocesses... Running the workflow"
-        print "=" * 60
-
-    @staticmethod
-    def print_log_if_exists_old(log_file, header):
-        if not isfile(log_file):
-            return
-        with open(log_file, "r") as f:
-            content = f.read()
-            if len(content) > 1:
-                print "--- {} : {}".format(header, "-" * (60 - len(header) - 7))
-                print content
-
-    @staticmethod
-    def print_logs_old(process):
-        WorkflowRuner.print_log_if_exists(process.log_stdout, "stdout")
-        WorkflowRuner.print_log_if_exists(process.log_stderr, "stderr")
-
-    @staticmethod
-    def list_extensions():
-        path = join(WorkflowRuner._extensions_dir, '*')
-        return glob(path)
-
-    @staticmethod
-    def get_logger():
-        return LogsFollower.get_logger()
-
-    @staticmethod
     def resources2list(resources):
         res = "\n".join(("* {}".format(resource.url) for resource in resources))
         return res
@@ -233,7 +152,97 @@ class WorkflowRuner():
             finally:
                 self._pool.terminate()
                 self._pool.join()
+                WorkflowRuner.mark_unfinished_processes_as_failure(workflow)
         return nb_process_run
+
+    @staticmethod
+    def mark_unfinished_processes_as_failure(workflow):
+        for process in workflow.iter_processes():
+            if process.start and not process.end:
+                process.set_end(False)
+        workflow.dump()
+        workflow.create_reports()
+
+    @staticmethod
+    def prepare_and_assign_paths(process):
+        log_stdout = join(WorkflowRuner._logs_dir, "{}_stdout.txt".format(process.id))
+        log_stderr = join(WorkflowRuner._logs_dir, "{}_err.txt".format(process.id))
+        # It would be a good idea to clean up all directories before
+        # running the whole workflow
+        # For the moment we clean here : before folowing the logs
+        if isfile(log_stdout):
+            remove(log_stdout)
+        if isfile(log_stderr):
+            remove(log_stderr)
+        reserved_path = join(WorkflowRuner._processes_dir, process.id)
+        if isdir(reserved_path):
+            rmtree(reserved_path)
+        elif isfile(reserved_path):
+            remove(reserved_path)
+        process.assign_paths(reserved_path, log_stdout, log_stderr)
+
+    @staticmethod
+    def create_tuttle_dirs():
+        if not isdir(WorkflowRuner._processes_dir):
+            makedirs(WorkflowRuner._processes_dir)
+        if not isdir(WorkflowRuner._logs_dir):
+            makedirs(WorkflowRuner._logs_dir)
+
+    @staticmethod
+    def empty_extension_dir():
+        if not isdir(WorkflowRuner._extensions_dir):
+            makedirs(WorkflowRuner._extensions_dir)
+        else:
+            rmtree(WorkflowRuner._extensions_dir)
+            makedirs(WorkflowRuner._extensions_dir)
+
+    @staticmethod
+    def print_header(process, logger):
+        logger.info("=" * 60)
+        logger.info(process.id)
+        logger.info("=" * 60)
+
+    @staticmethod
+    def print_preprocess_header(process, logger):
+        logger.info("-" * 60)
+        logger.info("Preprocess : {}".format(process.id))
+        logger.info("-" * 60)
+
+    @staticmethod
+    def print_preprocesses_header():
+        print "=" * 60
+        print "Running preprocesses for this workflow"
+        print "=" * 60
+
+    @staticmethod
+    def print_preprocesses_footer():
+        print "=" * 60
+        print "End of preprocesses... Running the workflow"
+        print "=" * 60
+
+    @staticmethod
+    def print_log_if_exists_old(log_file, header):
+        if not isfile(log_file):
+            return
+        with open(log_file, "r") as f:
+            content = f.read()
+            if len(content) > 1:
+                print "--- {} : {}".format(header, "-" * (60 - len(header) - 7))
+                print content
+
+    @staticmethod
+    def print_logs_old(process):
+        WorkflowRuner.print_log_if_exists(process.log_stdout, "stdout")
+        WorkflowRuner.print_log_if_exists(process.log_stderr, "stderr")
+
+    @staticmethod
+    def list_extensions():
+        path = join(WorkflowRuner._extensions_dir, '*')
+        return glob(path)
+
+    @staticmethod
+    def get_logger():
+        return LogsFollower.get_logger()
 
 
 class TuttleEnv(EnvVar):
