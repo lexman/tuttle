e4d5d6ffcba8aa2cdd6a8144ef7e6e5040924a50
lexman <android@lexman.org>
2018-03-16 21:34:22 +0100

    Separated tuttle main entrypoint from tuttle-extend-workflow in order to launch tuttle-extend-workflow twice as fast

--- tuttle/entry_points/tuttle_main.py
+++ tuttle/entry_points/tuttle_main.py
@@ -7,7 +7,6 @@ from argparse import ArgumentParser, ArgumentTypeError
 from tuttle.commands import run, invalidate
 from tuttle.utils import CurrentDir
 from tuttle.version import version
-from tuttle.extend_workflow import extend_workflow, ExtendError, extract_variables
 
 
 def check_minus_1_or_positive(value):
@@ -86,31 +85,3 @@ def format_variable(name, value):
     else:
         res = "{}={}".format(name, value)
     return res
-
-
-def tuttle_extend_workflow_main():
-    parser = ArgumentParser(
-        description="Extends a workflow by adding a templated tuttle project. Must be run from a preprocessor in a "
-                    "tuttle project - version {}".format(version)
-    )
-    parser.add_argument("-v", "--verbose", action="store_true",
-                        help="Display template and variables")
-    parser.add_argument("template", help="template file")
-    parser.add_argument('variables', help='variables to insert into the template int the form my_var="my value"',
-                        nargs="*")
-    parser.add_argument('-n', '--name',
-                        default='extension',
-                        dest='name',
-                        help='Name of the extended workflow')
-    params = parser.parse_args()
-
-    try:
-        vars_dic = extract_variables(params.variables)
-        extend_workflow(params.template, name=params.name, **vars_dic)
-        if params.verbose:
-            print("Injecting into template {} :".format(params.template))
-            for key, value in vars_dic.iteritems():
-                print(" * {}".format(format_variable(key, value)))
-    except ExtendError as e:
-        print e.message
-        exit(1)
